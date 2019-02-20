6361d6bac87c201a763030ce4e01ac1e75c4e949
Matthew Meade <mafumeade@gmail.com>
2019-02-19 22:43:44 -0330

    Fix error handling

--- routes/catchErrors.js
+++ routes/catchErrors.js
@@ -2,6 +2,9 @@
 // Wraps async functions in a catch
 module.exports = catchErrors = fn => {
   return function(req, res, next) {
-    return fn(req, res, next).catch(err => res.status(400).json(err));
+    return fn(req, res, next).catch(err => {
+      console.log(err);
+      res.status(400).send(err.toString());
+    });
   };
 };
