ab676728eb43fbede06007033d788dfa8c963b49
Matthew Meade <mafumeade@gmail.com>
2019-02-19 22:31:04 -0330

    Fix find user promise

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,7 +11,7 @@ module.exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
   // Check if account already exists with submitted email
-  if (findUser({ email })) {
+  if (await findUser({ email })) {
     return res.status(400).json({ email: "Email already in use" });
   }
 
@@ -20,15 +20,14 @@ module.exports.registerUser = async (req, res) => {
   const hash = bcrypt.hashSync(password, salt);
 
   // Save user
-  const newUser = await new User({ name, email, password: hash });
+  const newUser = await new User({ name, email, password: hash }).save();
 
-  // Respond with new user
-  delete newUser.passport; // Hide password from response
-  res.json(newUser);
+  // Respond with success
+  res.json({ message: "Success" });
 };
 
-async function findUser(query) {
-  return await User.findOne(query);
+function findUser(query) {
+  return User.findOne(query);
 }
 
 // Login a user
@@ -38,7 +37,7 @@ module.exports.loginUser = async (req, res) => {
   const user = await fundUser({ email });
 
   // Check if account exists
-  if (!findUser({ email })) {
+  if (!(await findUser({ email }))) {
     return res.status(400).json({ email: "No user found with that email" });
   }
 
