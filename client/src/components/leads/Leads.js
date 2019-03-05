0cae4bc8a8bf5debd96ad88c16eff1e31b815cda
Matthew Meade <mafumeade@gmail.com>
2019-03-05 03:29:38 -0330

    Add click event to lead list rows

--- client/src/components/leads/Leads.js
+++ client/src/components/leads/Leads.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import Spinner from "../common/Spinner";
 import { getLeads, refreshLeads } from "../../actions/leadActions";
 
+import { withRouter } from "react-router-dom";
+
 class Leads extends Component {
   componentDidMount() {
     this.props.getLeads();
@@ -11,6 +13,10 @@ class Leads extends Component {
 
   onDeleteClick = () => {
     this.props.refreshLeads();
+  };
+
+  onRowClick = id => {
+    this.props.history.push("/lead/" + id);
   };
 
   render() {
@@ -29,7 +35,8 @@ class Leads extends Component {
       );
     } else {
       leadsContent = leads.map(lead => (
-        <tr key={lead._id}>
+        // Can't wrap tr in Link, using onRowClick instead
+        <tr key={lead._id} onClick={this.onRowClick.bind(this, lead._id)}>
           <td>{lead.name}</td>
           <td>{lead.email}</td>
           <td>{lead.phone}</td>
@@ -73,7 +80,9 @@ const mapStateToProps = state => ({
   loading: state.leadList.loading,
 });
 
-export default connect(
-  mapStateToProps,
-  { getLeads, refreshLeads }
-)(Leads);
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { getLeads, refreshLeads }
+  )(Leads)
+);
