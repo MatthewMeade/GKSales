a153b1dd2c60c32f49ac863bce2cb93dc379ea19
Matthew Meade <mafumeade@gmail.com>
2019-03-05 03:29:58 -0330

    Add links to navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,42 +18,15 @@ class Navbar extends Component {
     const { isAuthenticated, user } = this.props.auth;
 
     const authLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/feed">
-            Post Feed
-          </Link>
-        </li>
-        <li className="nav-item">
+      <ul className="navbar-nav">
+        <li className="nav-item ml-3">
           <Link className="nav-link" to="/dashboard">
             Dashboard
           </Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" onClick={this.onLogoutClick} href="/">
-            <img
-              src={user.avatar}
-              alt={user.name}
-              style={{ width: "25px", margiRight: "5px" }}
-              title="You must have a Gravatar connected to your email to display an image"
-              className="rounded-circle"
-            />{" "}
-            Logout
-          </a>
-        </li>
-      </ul>
-    );
-
-    const guestLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">
-            Sign Up
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">
-            Login
+        <li className="nav-item ml-3">
+          <Link className="nav-link" to="/leads">
+            Leads
           </Link>
         </li>
       </ul>
@@ -66,21 +39,7 @@ class Navbar extends Component {
           Garage King Sales
         </Link>
 
-        {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-          <span className="navbar-toggler-icon" />
-        </button>
-
-        <div className="collapse navbar-collapse" id="mobile-nav">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/profiles">
-                Developers
-              </Link>
-            </li>
-          </ul>
-
-          {isAuthenticated ? authLinks : guestLinks}
-        </div> */}
+        {isAuthenticated && authLinks}
       </nav>
     );
   }
