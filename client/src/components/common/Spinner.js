53cba46b57ad3c88c81584a3ca01336ad612402b
Jordan Pardy <jordanpardy@protonmail.com>
2019-03-13 12:41:39 -0230

    Changed spinner to transparent version

--- client/src/components/common/Spinner.js
+++ client/src/components/common/Spinner.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import spinner from './spinner.gif';
+import React from "react";
+import spinner from "./spinner.gif";
+import "./Spinner.scss";
 
 export default () => {
   return (
     <div>
-      <img
-        src={spinner}
-        style={{ width: '200px', margin: 'auto', display: 'block' }}
-        alt="Loading..."
-      />
+      <div className="sk-circle">
+        <div className="sk-circle1 sk-child" />
+        <div className="sk-circle2 sk-child" />
+        <div className="sk-circle3 sk-child" />
+        <div className="sk-circle4 sk-child" />
+        <div className="sk-circle5 sk-child" />
+        <div className="sk-circle6 sk-child" />
+        <div className="sk-circle7 sk-child" />
+        <div className="sk-circle8 sk-child" />
+        <div className="sk-circle9 sk-child" />
+        <div className="sk-circle10 sk-child" />
+        <div className="sk-circle11 sk-child" />
+        <div className="sk-circle12 sk-child" />
+      </div>
     </div>
   );
 };
